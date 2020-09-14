7f0925b95c23213ec8a322ea0839400885dc54db
Magnus Hansen <magnus131hansen@gmail.com>
2020-09-14 09:23:38 +0200

    Done Done

---
+++ EchoClient/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello world!");
+
+            Client.Start("localhost");
+        }
+    }
+
+    class Client
+    {
+        public static void Start(string server)
+        {
+            try
+            {
+                Int32 port = 7777;
+                TcpClient socket = new TcpClient(server, port);
+                NetworkStream ns = null;
+                string message = "";
+
+                while (message != "exit")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Write dumbass:");
+
+                    message = Console.ReadLine();
+
+                    Byte[] data = Encoding.ASCII.GetBytes(message);
+
+                    ns = socket.GetStream();
+
+                    ns.Write(data, 0, data.Length);
+
+                    Console.WriteLine("Sent: {0}", message);
+
+                    data = new Byte[256];
+
+                    String responseData = String.Empty;
+
+                    Int32 bytes = ns.Read(data, 0, data.Length);
+                    responseData = Encoding.ASCII.GetString(data, 0, bytes);
+                    Console.WriteLine("Received: {0}", responseData);
+                }
+                ns.Close();
+                socket.Close();
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+    }
+}
--- EchoServer/Program.cs
+++ EchoServer/Program.cs
@@ -1,12 +1,85 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace EchoServer
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
+            Server.Start();
+        }
+
+    }
+
+    class Server
+    {
+        public static void Start()
+        {
+            TcpListener server = null;
+            int clientsConnected = 0;
+            try
+            {
+                Int32 port = 7777;
+                IPAddress localAddress = IPAddress.Loopback;
+
+                server = new TcpListener(localAddress, port);
+
+                server.Start();
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection");
+
+                    TcpClient socket = server.AcceptTcpClient();
+                    clientsConnected++;
+                    Console.WriteLine($"Client {clientsConnected} has connected to the server");
+                    Task.Run(() => { DoClient(socket ,$"Client {clientsConnected}"); });
+                    //socket.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+
+        private static async void DoClient(TcpClient socket, string client)
+        {
+            Byte[] bytes = new byte[256];
+            string data = null;
+
+            NetworkStream ns = socket.GetStream();
+
+            int i;
+
+            try
+            {
+                while (socket.Connected && (i = await ns.ReadAsync(bytes, 0, bytes.Length)) != 0)
+                {
+                    data = Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine($"{client} received: {0}", data);
+
+                    Byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    await ns.WriteAsync(msg, 0, msg.Length);
+                    Console.WriteLine($"{client} sent: {0}", data);
+                }
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine("Exception: {0}", e.Message);
+                Console.WriteLine($"{client} has disconnected from the server");
+                socket.Dispose();
+                socket.Close();
+            }
         }
     }
 }
